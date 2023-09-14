658b619a082651b9d560ce282294bf5977cc19ab
Shariful Islam <70468977+Sharif355@users.noreply.github.com>
2023-09-15 01:38:27 +0600

    card section updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { FiDollarSign } from 'react-icons/fa';
+import { FiDollarSign,FiBookOpen } from "react-icons/fi";
 import { useEffect, useState } from 'react'
 import './App.css'
 
@@ -16,23 +16,35 @@ function App() {
   return (
     <>
       
-      <h1 className='text-3xl text-center font-bold mt-10 ' >Course Registration</h1>
-      <div className='grid grid-cols-3 gap-3 container mx-auto' >
+      <h1 className='text-3xl text-center font-bold my-10 ' >Course Registration</h1>
+      <div className="flex justify-center gap-5  container  mx-auto " >
+        {/* Card Section */}
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 ' >
        {
         courses.map(course =>(
-          <div key={course.id} className='w-[312px] p-5  border-2 rounded-lg space-y-2 flex flex-col justify-center  ' >
-          <img className='w-[280px] h-[200px] pb-5 '  src={course.cover} alt="" />
+          <div key={course.id} className='w-96 p-5  border-2 rounded-lg space-y-2    ' >
+          <img className='w-96 h-80 pb-5 '  src={course.cover} alt="" />
           <h3 className='font-bold' >{course.title}</h3>
-          <p>{course.description}</p>
-          <div className='Pri-Cre' >
-            <FiDollarSign></FiDollarSign>
-            <p>  Price: {course.price} </p>
-            <p>Credit: {course.credit_hour} </p>
+          <p  >{course.description}</p>
+          <div className='flex justify-between text-lg ' >
+             <div className="flex items-center" >
+             <FiDollarSign></FiDollarSign>
+            <p className="pl-3" >  Price: {course.price} </p>
+             </div>
+           <div className="flex items-center ">
+            <FiBookOpen></FiBookOpen>
+           <p className="pl-3" >Credit: {course.credit_hour}hr </p>
+           </div>
           </div>
-          <button className='btn bg-blue-500 text-white ' >Select</button>
+          <button className='btn bg-blue-500 text-white w-full ' >Select</button>
         </div>
         ))
        }
+      </div>
+      {/* Cart Section */}
+      <div>
+          <h1>This is Cart</h1>
+      </div>
       </div>
      
     </>
