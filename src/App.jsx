ae5399b0f13ba6959a59a527689d49976057ec7f
Shariful Islam <70468977+Sharif355@users.noreply.github.com>
2023-09-14 19:00:16 +0600

    React Folder added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-3xl text-center font-bold mt-10 ' >Course Registration</h1>
+      
+     
+    </>
+  )
+}
+
+export default App
