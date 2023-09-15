4515b0bd0cf8af62a4f68401fa4ff2882a8aed05
Shariful Islam <70468977+Sharif355@users.noreply.github.com>
2023-09-15 13:07:36 +0600

    Total Hour & Remaining credit section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { FiDollarSign,FiBookOpen } from "react-icons/fi";
 import { useEffect, useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css'
 import Cart from "./Components/Cart/Cart";
 
@@ -19,18 +21,35 @@ function App() {
   },[])
   
  
-  const handleNewCart = (course) => {
-    let hour = course.credit_hour;
-    let total = course.price;
-   newCourse.forEach(course=>{
+  const handleNewCart = (courses) => {
+    let hour = courses.credit_hour;
+    let total = courses.price;
+    const isExist = newCourse.find(course=> course.id == courses.id )
+    if(isExist){
+      return toast('This Course already Added at Cart!')
+    }
+    else{
+       newCourse.forEach(course=>{
     hour= hour+course.credit_hour;
     total = total + course.price;
    })
    const remaining = 20-hour;
-   setRemainingHour(remaining)
-   setTotalHour(hour)
+   if(remaining < 0){
+    return toast("You don't have enough Credit/hr")
+   }
+   else{
+    setRemainingHour(remaining)
+   }
+   if (hour > 20 ) {
+    return toast('You not allowed to take more than 20 Credit/hr')
+   }
+   else{
+    setTotalHour(hour)
+   }
    setTotalAmount(total)
-    setNewCourse([...newCourse,course])
+    setNewCourse([...newCourse,courses])
+    }
+  
   }
 
   return (
@@ -57,15 +76,24 @@ function App() {
            </div>
           </div>
           <button onClick={()=>handleNewCart(course)} className='btn bg-blue-500 text-white w-full ' >Select</button>
+          
         </div>
         ))
        }
       </div>
       {/* Cart Section */}
-      <div >
+      <div className=' rounded-lg p-5 ' >
+      <h1 className='text-blue-500 font-medium pb-3' >Credit Hour Remaining {remainingHour}hr </h1>
+            <hr />
+             <h1 className='font-bold py-3' >Course Name</h1>
         {
-          newCourse.map((course,idx)=><Cart key={idx} totalAmount={totalAmount}  remainingHour={remainingHour}  totalHour={totalHour}  course = {course} ></Cart>)
+          newCourse.map((course)=><Cart key={course.id}   course = {course} ></Cart>)
         }
+        <hr />
+         <h1 className='py-3'>Total Credit Hour: {totalHour}  </h1>
+             <hr />
+             <h1 className='py-3'>Total Price: {totalAmount} USD</h1>
+        <ToastContainer/>
       </div>
       </div>
      
--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,28 +1,22 @@
 import PropTypes from 'prop-types';
 
-const Cart = ({course,totalHour,remainingHour,totalAmount}) => {
+const Cart = ({course}) => {
     
     return (
-        <div  className='border-2 rounded-lg p-5' >
-            <h1 className='text-blue-500 font-medium pb-3' >Credit Hour Remaining {remainingHour}hr </h1>
-            <hr />
-             <h1 className='font-bold py-3' >Course Name</h1>
-             <ol className='pb-3' >
+        <div>
+           
+             <ol  className='pb-3' >
                 <li>{course.title}</li>
              </ol>
-             <hr />
-             <h1 className='py-3'>Total Credit Hour: {totalHour}  </h1>
-             <hr />
-             <h1 className='py-3'>Total Price: {totalAmount} USD</h1>
+             
+            
         </div>
     );
 };
 
 Cart.propTypes = {
     course: PropTypes.object.isRequired,
-    totalHour: PropTypes.number.isRequired,
-    remainingHour:PropTypes.number.isRequired,
-    totalAmount:PropTypes.number.isRequired
+   
 }
 
 export default Cart;
